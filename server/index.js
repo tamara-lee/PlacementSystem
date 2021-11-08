e65bff88f4a920641dcc899d8e83810d77b38a29
Qianyi <qian8824@connect.hku.hk>
2021-11-08 15:49:49 +0800

    connection to MySQL database-index.js

--- server/index.js
+++ server/index.js
@@ -5,12 +5,34 @@ const app = express();
 
 app.use(express.json());
 
-const db = mysql.createConnection({
-    user: "root",
-    host: "localhost",
-    password: "password",
-    database: "placementsystem"
-})
+//Create connection
+var connection = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  // password: "admin",
+  //database: "login_db",
+  password: "password",
+  database: "placementsystem",
+  port: "3306"
+});
+
+//Connect to MySQL
+connection.connect(err => {
+  if (err) {
+      throw err;
+  }
+  console.log("MySQL Connected");
+});
+
+
+// connection.query("CREATE TABLE tabletest2(id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY, thing VARCHAR(255) NOT NULL)", (err, rows) => {
+//   if (err) {
+//       throw err;
+//   } else {
+//       console.log("Data Sent");
+//       console.log(rows)
+//   }
+// });
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
