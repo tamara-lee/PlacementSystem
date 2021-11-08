81738e2b49929a0be7e20f773e2d6893ae150142
Qianyi <qian8824@connect.hku.hk>
2021-11-08 21:31:19 +0800

    authenticating if user who logs in from frontend (login page) exists in the mySQL database - index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mysql = require("mysql");
+const cors = require("cors");
 
 const app = express();
 
+//automatically parse every json object from the frontend
 app.use(express.json());
+app.use(cors());
 
 //Create connection
 var connection = mysql.createConnection({
@@ -13,7 +16,7 @@ var connection = mysql.createConnection({
   //database: "login_db",
   password: "password",
   database: "placementsystem",
-  port: "3306"
+  //port: "3306"
 });
 
 //Connect to MySQL
@@ -25,7 +28,7 @@ connection.connect(err => {
 });
 
 
-// connection.query("CREATE TABLE tabletest2(id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY, thing VARCHAR(255) NOT NULL)", (err, rows) => {
+// connection.query("CREATE TABLE tabletest(id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY, thing VARCHAR(255) NOT NULL)", (err, rows) => {
 //   if (err) {
 //       throw err;
 //   } else {
@@ -34,9 +37,46 @@ connection.connect(err => {
 //   }
 // });
 
+//query to insert a new user to the database
+// app.post('/register', (req, res)=> {
+
+//   const username = req.body.username
+//   const password = req.body.password
+
+//   connection.query(
+//     "INSERT INTO users (username,password) VALUES (?,?)", 
+//     [username, password],
+//     (err, result) => {
+//       console.log(err);
+//     });
+// });
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
+  
+app.post('/login', (req, res)=> {
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  connection.query(
+    "SELECT * FROM Users WHERE username = ? AND password = ?", 
+    [username, password],
+    (err, result) => {
+
+      if (err) {
+        res.send( { err: err } );
+      } 
+      
+      if (result.length > 0) {
+        res.send(result);
+      } else {
+        res.send( { message: "Incorrect username or password" } );
+      }
+      
+    });
+});
   
 app.listen(3001, () => {
   console.log("Server Running")
