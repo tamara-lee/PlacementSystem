4ecd69648208b8e36a30e1793129dd1a7aaff578
Qianyi <qian8824@connect.hku.hk>
2021-11-09 11:57:08 +0800

    creating cookies and sessions - indeex.js

--- server/index.js
+++ server/index.js
@@ -2,11 +2,31 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 
+//const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
 const app = express();
 
 //automatically parse every json object from the frontend
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST"],
+  credentials: true //allowing the cookie to be enabled
+}));
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+  key: "userId", //name of cookie we're gonna create
+  secret: "secretKey",//to be changed
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: 60 * 60 * 24 * 1000 //can stay logged in for 24hours. in milliseconds
+  }
+}))
 
 //Create connection
 var connection = mysql.createConnection({
@@ -70,6 +90,8 @@ app.post('/login', (req, res)=> {
       } 
       
       if (result.length > 0) {
+        req.session.user = result;
+        console.log(req.session.user);
         res.send(result);
       } else {
         res.send( { message: "Incorrect username or password" } );
