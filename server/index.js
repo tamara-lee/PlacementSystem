c49f5cdc6f52de6f5d70a04d81a0dd3beb6f0872
LAPTOP-R3SEKBDG\Tamara <tamara6@connect.hku.hk>
2021-11-22 20:30:57 +0800

    with update to models index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const mysql = require("mysql");
 const cors = require("cors");
+
+const mysql = require("mysql");
+const db = require("./models");
+const { User } = require("./models");
 
 //const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -10,52 +13,43 @@ const app = express();
 
 //automatically parse every json object from the frontend
 app.use(express.json());
-app.use(cors({
-  origin: ["http://localhost:3000"],
-  methods: ["GET", "POST"],
-  credentials: true //allowing the cookie to be enabled
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true, //allowing the cookie to be enabled
+  })
+);
 app.use(cookieParser());
-app.use(express.urlencoded( { extended: true } ));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
-  key: "userId", //name of cookie we're gonna create
-  secret: "secretKey",//to be changed
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    expires: 60 * 60 * 24 * 1000 //can stay logged in for 24hours. in milliseconds
-  }
-}))
+app.use(
+  session({
+    key: "userId", //name of cookie we're gonna create
+    secret: "secretKey", //to be changed
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24 * 1000, //can stay logged in for 24hours. in milliseconds
+    },
+  })
+);
 
 //Create connection
-var connection = mysql.createConnection({
+/* var connection = mysql.createConnection({
   user: "root",
   host: "localhost",
-  // password: "admin",
-  //database: "login_db",
   password: "password",
   database: "placementsystem",
-  //port: "3306"
-});
+}); */
 
 //Connect to MySQL
-connection.connect(err => {
+/* connection.connect(err => {
   if (err) {
       throw err;
   }
   console.log("MySQL Connected");
-});
-
-
-// connection.query("CREATE TABLE tabletest(id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY, thing VARCHAR(255) NOT NULL)", (err, rows) => {
-//   if (err) {
-//       throw err;
-//   } else {
-//       console.log("Data Sent");
-//       console.log(rows)
-//   }
-// });
+}); */
 
 //query to insert a new user to the database
 // app.post('/register', (req, res)=> {
@@ -64,51 +58,173 @@ connection.connect(err => {
 //   const password = req.body.password
 
 //   connection.query(
-//     "INSERT INTO users (username,password) VALUES (?,?)", 
+//     "INSERT INTO users (username,password) VALUES (?,?)",
 //     [username, password],
 //     (err, result) => {
 //       console.log(err);
 //     });
 // });
 
-app.get("/api", (req, res) => {
+/* app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
-
-app.get("/login", (req, res)=> {
+ */
+/* app.get("/login", (req, res)=> {
   //question if there is a session with user object in our server
   if (req.session.user){
     res.send({ loggedIn: true, user: req.session.user });
   } else {
     res.send({ loggedIn: false });
   }
-})
-  
-app.post('/login', (req, res)=> {
+}) */
 
-  const username = req.body.username;
-  const password = req.body.password;
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-  connection.query(
-    "SELECT * FROM Users WHERE username = ? AND password = ?", 
-    [username, password],
-    (err, result) => {
+  const user = await User.findOne({ where: { username: username } });
 
-      if (err) {
-        res.send( { err: err } );
-      } 
-      
-      if (result.length > 0) {
-        req.session.user = result;
-        console.log(req.session.user);
-        res.send(result);
-      } else {
-        res.send( { message: "Incorrect username or password" } );
-      }
-      
-    });
+  if (!user) res.status(400).json({ error: "User does not exist." });
+
+  res.json("Logged In");
+
+  //const username = req.body.username;
+  //const password = req.body.password;
+
+  // connection.query(
+  //   "SELECT * FROM Users WHERE username = ? AND password = ?",
+  //   [username, password],
+  //   (err, result) => {
+
+  //     if (err) {
+  //       res.send( { err: err } );
+  //     }
+
+  //     if (result.length > 0) {
+  //       req.session.user = result;
+  //       console.log(req.session.user);
+  //       res.send(result);
+  //     } else {
+  //       res.send( { message: "Incorrect username or password" } );
+  //     }
+
+  //   });
 });
-  
-app.listen(3001, () => {
-  console.log("Server Running")
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Server Running");
+  });
 });
+
+// const express = require("express");
+// const mysql = require("mysql");
+// const cors = require("cors");
+
+// //const bodyParser = require("body-parser");
+// const cookieParser = require("cookie-parser");
+// const session = require("express-session");
+
+// const app = express();
+
+// //automatically parse every json object from the frontend
+// app.use(express.json());
+// app.use(cors({
+//   origin: ["http://localhost:3000"],
+//   methods: ["GET", "POST"],
+//   credentials: true //allowing the cookie to be enabled
+// }));
+// app.use(cookieParser());
+// app.use(express.urlencoded( { extended: true } ));
+
+// app.use(session({
+//   key: "userId", //name of cookie we're gonna create
+//   secret: "secretKey",//to be changed
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: {
+//     expires: 60 * 60 * 24 * 1000 //can stay logged in for 24hours. in milliseconds
+//   }
+// }))
+
+// //Create connection
+// var connection = mysql.createConnection({
+//   user: "root",
+//   host: "localhost",
+//   // password: "admin",
+//   //database: "login_db",
+//   password: "password",
+//   database: "placementsystem",
+//   //port: "3306"
+// });
+
+// //Connect to MySQL
+// connection.connect(err => {
+//   if (err) {
+//       throw err;
+//   }
+//   console.log("MySQL Connected");
+// });
+
+// // connection.query("CREATE TABLE tabletest(id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY, thing VARCHAR(255) NOT NULL)", (err, rows) => {
+// //   if (err) {
+// //       throw err;
+// //   } else {
+// //       console.log("Data Sent");
+// //       console.log(rows)
+// //   }
+// // });
+
+// //query to insert a new user to the database
+// // app.post('/register', (req, res)=> {
+
+// //   const username = req.body.username
+// //   const password = req.body.password
+
+// //   connection.query(
+// //     "INSERT INTO users (username,password) VALUES (?,?)",
+// //     [username, password],
+// //     (err, result) => {
+// //       console.log(err);
+// //     });
+// // });
+
+// app.get("/api", (req, res) => {
+//     res.json({ message: "Hello from server!" });
+//   });
+
+// app.get("/login", (req, res)=> {
+//   //question if there is a session with user object in our server
+//   if (req.session.user){
+//     res.send({ loggedIn: true, user: req.session.user });
+//   } else {
+//     res.send({ loggedIn: false });
+//   }
+// })
+
+// app.post('/login', (req, res)=> {
+
+//   const username = req.body.username;
+//   const password = req.body.password;
+
+//   connection.query(
+//     "SELECT * FROM Users WHERE username = ? AND password = ?",
+//     [username, password],
+//     (err, result) => {
+
+//       if (err) {
+//         res.send( { err: err } );
+//       }
+
+//       if (result.length > 0) {
+//         req.session.user = result;
+//         console.log(req.session.user);
+//         res.send(result);
+//       } else {
+//         res.send( { message: "Incorrect username or password" } );
+//       }
+
+//     });
+// });
+
+// app.listen(3001, () => {
+//   console.log("Server Running")
+// });
