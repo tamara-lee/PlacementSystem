c7b2211eaaa090168083d3c59a8fca638c9db1d3
LAPTOP-R3SEKBDG\Tamara <tamara6@connect.hku.hk>
2021-11-23 23:06:03 +0800

    updated myplacementrecord skeleton

--- client/src/Pages/Student/MyPlacementRecord/MyPlacementRecord.js
+++ client/src/Pages/Student/MyPlacementRecord/MyPlacementRecord.js
@@ -1,107 +1,149 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../../../components/NavBar/NavBar";
 import Navbar from "../../../components/NavBar/index";
 import { Redirect } from "react-router-dom";
 import "./style.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function MyPlacementRecord({ authorized }) {
   // if (authorized === false) {
   //   console.log(authorized);
   //   return <Redirect to="/" />;
   // }
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const uploadAppointmentLetter = () => {
+    console.log("Upload appointment letter button clicked!");
+  };
+
+  const uploadFeedbackForm = () => {
+    console.log("Upload feedback button clicked!");
+  };
+
+  const submitForm = () => {
+    console.log("Submit button clicked!");
+  };
+
   return (
     <>
       <Navbar />
       <div className="container">
-        <div className="row">
-          <div className="column">
-            <form>
-              <p>
-                <label for="studentNo">Student No.</label>
+        <form className="form-table">
+          <div className="row">
+            <div className="column">
+              <p className="table-row">
+                <label htmlFor="studentNo">Student No.</label>
                 <input type="text" id="studentNo" />
               </p>
-              <p>
-                <label for="studentName">Student Name</label>
+              <p className="table-row">
+                <label htmlFor="studentName">Student Name</label>
                 <input type="text" id="studentName" />
               </p>
-              <p>
-                <label for="curriculum">Curriculum</label>
+              <p className="table-row">
+                <label htmlFor="curriculum">Curriculum</label>
                 <input type="text" id="curriculum" />
               </p>
-              <p>
-                <label for="companyName">Company Name</label>
+              <p className="table-row">
+                <label htmlFor="companyName">Company Name</label>
                 <input type="text" id="companyName" />
               </p>
-              <p>
-                <label for="jobTitle">Job Title / Position</label>
+              <p className="table-row">
+                <label htmlFor="jobTitle">Job Title / Position</label>
                 <input type="text" id="jobTitle" />
               </p>
-              <p>
-                <label for="jobNature">Job Nature</label>
+              <p className="table-row">
+                <label htmlFor="jobNature">Job Nature</label>
                 <input type="text" id="jobNature" />
               </p>
-              <p>
-                <label for="startDate">Start Date</label>
-                <input type="date" id="startDate" />
-              </p>
-              <p>
-                <label for="endDate">End Date</label>
-                <input type="date" id="endDate" />
-              </p>
-              <p>
-                <label for="location">Working Location</label>
+              <div className="table-row">
+                <label htmlFor="startDate">Start Date</label>
+                <DatePicker
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                  id="startDate"
+                />
+              </div>
+              <div className="table-row">
+                <label htmlFor="endDate">End Date</label>
+                <DatePicker
+                  selected={endDate}
+                  onChange={(date) => setEndDate(date)}
+                  id="endDate"
+                />
+              </div>
+              <p className="table-row">
+                <label htmlFor="location">Working Location</label>
                 <input type="text" id="location" />
               </p>
-              <p>
-                <label for="paymentType">Payment Type</label>
+              <p className="table-row">
+                <label htmlFor="paymentType">Payment Type</label>
                 <select name="paymentType" id="paymentType">
                   <option value="unpaid">Unpaid</option>
                   <option value="paid">Paid</option>
                 </select>
               </p>
-            </form>
-          </div>
-          <div className="column">
-            <form>
-              <p>
-                <label for="salary">Salary</label>
+            </div>
+            <div className="column">
+              <p className="table-row">
+                <label htmlFor="salary">Salary</label>
                 <input type="text" id="salary" />
               </p>
-              <p>
-                <label for="supervisorName">Supevisor Name</label>
+              <p className="table-row">
+                <label htmlFor="supervisorName">Supevisor Name</label>
                 <input type="text" id="supervisorName" />
               </p>
-              <p>
-                <label for="supervisorPhone">Supervisor Telephone</label>
+              <p className="table-row">
+                <label htmlFor="supervisorPhone">Supervisor Telephone</label>
                 <input type="tel" id="supervisorPhone" />
               </p>
-              <p>
-                <label for="supervisorEmail">Supervisor Email</label>
+              <p className="table-row">
+                <label htmlFor="supervisorEmail">Supervisor Email</label>
                 <input type="email" id="supervisorEmail" />
               </p>
-              <p>
-                <label for="appointmentLetter">Appointment Letter</label>
-                <button id="appointmentLetter">Upload</button>
+              <p className="table-row">
+                <label htmlFor="appointmentLetter">Appointment Letter</label>
+                <button
+                  id="appointmentLetter"
+                  onClick={uploadAppointmentLetter}
+                >
+                  <span>Upload</span>
+                </button>
               </p>
-              <p>
-                <label for="feedbackForm">Feedback Form</label>
-                <button id="feedbackForm">Upload</button>
+              <p className="table-row">
+                <label htmlFor="feedbackForm">Feedback Form</label>
+                <button id="feedbackForm" onClick={uploadFeedbackForm}>
+                  <span>Upload</span>
+                </button>
               </p>
-              <p>
-                <label for="feedbackComment">Feedback Comment</label>
+              <p className="table-row">
+                <label htmlFor="feedbackComment">Feedback Comment</label>
                 <input type="text" id="feedbackComment" />
               </p>
-              <p>
-                <label for="placementStatus">Placement Status</label>
+              <p className="table-row">
+                <label htmlFor="placementStatus">Placement Status</label>
                 <select name="placementStatus" id="placementStatus">
                   <option value="approved">Approved</option>
                   <option value="incomplete">Incomplete</option>
                   <option value="waiting">Waiting</option>
                 </select>
               </p>
-            </form>
+              <p className="table-row">
+                <label htmlFor="remarks">Remarks</label>
+                <input
+                  type="text"
+                  id="remarks"
+                  value="add chat box here"
+                  readOnly
+                />
+              </p>
+            </div>
           </div>
-        </div>
+          <button className="form-submit" onClick={submitForm}>
+            Save & Submit
+          </button>
+        </form>
       </div>
     </>
   );
