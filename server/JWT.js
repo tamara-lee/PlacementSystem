094faf27a92faff43e83a87faef1ebe4a2ec405c
Qianyi <qian8824@connect.hku.hk>
2021-11-24 16:08:29 +0800

    implemeted jwt tokens

--- server/JWT.js
+++ server/JWT.js
@@ -11,4 +11,25 @@ const createTokens = (user) => {
         return accessToken;
 };
 
-module.exports = { createTokens };
+//middleware: func that runs before a request
+const validateToken = (req, res, next) => {
+    const accessToken = req.cookies["access-token-cookie"];
+
+    if (!accessToken) {
+        return res.status(400).json( {error: "User is not authenticated!\nPlease log in."} );
+    }
+
+    try {
+        const validToken = verify(accessToken, "tempjwtsecretkey")
+        if (validToken) {
+            //create authenticate variable, and set it to true
+            req.authenticated = true;
+            //next() to move forward with the request
+            return next();
+        }
+    } catch(err) {
+        return res.status(400).json( {error: err} );
+    }
+};
+
+module.exports = { createTokens , validateToken };
